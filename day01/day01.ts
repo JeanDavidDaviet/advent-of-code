007aac4466184f425fcfd2fbb1559586c869a13e
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2020-12-15 14:55:11 +0100

    day01 improvements

--- day01/day01.ts
+++ day01/day01.ts
@@ -3,21 +3,31 @@ const input = await Deno.readFile("./input.txt");
 const lines = decoder.decode(input).trim().split("\n");
 const numbers = lines.map((x) => parseInt(x, 10));
 
-const getPart1 = (resolve: (value: [number, number]) => void) => {
+type ResolveArg = (value: [number, number]) => void;
+type RejectArg = (reason: Error) => void;
+
+enum Error {
+  NOT_FOUND = 'no item have a 2020 sum'
+}
+
+const getPart1 = (resolve: ResolveArg, reject: RejectArg) => {
   let result: [number, number];
   for (const number1 of numbers) {
     for (const number2 of numbers) {
       const sum = number1 + number2;
       if (sum === 2020) {
+        console.log(number1, number2);
+        
         const product = number1 * number2;
         result = [sum, product];
         resolve(result);
       }
     }
   }
+  reject(Error.NOT_FOUND);
 };
 
-const getPart2 = (resolve: (value: [number, number]) => void) => {
+const getPart2 = (resolve: ResolveArg, reject: RejectArg) => {
   let result: [number, number];
   for (const number1 of numbers) {
     for (const number2 of numbers) {
@@ -31,11 +41,12 @@ const getPart2 = (resolve: (value: [number, number]) => void) => {
       }
     }
   }
+  reject(Error.NOT_FOUND);
 };
 
 // part 1
 try {
-  const sumAndProduct = await new Promise(((resolve) => getPart1(resolve)));
+  const sumAndProduct = await new Promise((resolve: ResolveArg, reject: RejectArg) => getPart1(resolve, reject));
   console.log(sumAndProduct);
 } catch (e) {
   console.error(e);
@@ -43,8 +54,10 @@ try {
 
 // part 2
 try {
-  const sumAndProduct = await new Promise(((resolve) => getPart2(resolve)));
+  const sumAndProduct = await new Promise((resolve: ResolveArg, reject: RejectArg) => getPart2(resolve, reject));
   console.log(sumAndProduct);
 } catch (e) {
   console.error(e);
 }
+
+export {}
