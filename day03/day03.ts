ad7b37e231c256ccba31a24299b6c885d8c3b13e
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2020-12-15 14:54:49 +0100

    day 03 part 2

--- day03/day03.ts
+++ day03/day03.ts
@@ -3,31 +3,46 @@ const lines = await getLinesFromFile('./input.txt');
 
 const cols = lines[0].length;
 const rows = lines.length;
-let trees = 0;
-let row = 1;
 
-interface Position {
+type Position = Slope | Square;
+
+interface Slope {
+  x: number,
+  y: number
+}
+
+interface Square {
   x: number;
   y: number;
 }
 
-const position : Position = { x: 0, y: 0 }
-
 enum TileType {
-  OPEN = 'open',
-  TREE = 'tree'
+  OPEN = '.',
+  TREE = '#'
 }
 
-while(row <= rows - 1){
-  position.x = (position.x + 3) % cols;
-  position.y += 1;
-  
-  if(lines[position.y][position.x] === '#'){
-    trees++;
+const runThrough = (slope: Position) : number => {
+  let row = 1;
+  let trees = 0;
+  const position : Position = { x: 0, y: 0}
+
+  while(row <= rows - 1){
+    position.x = (position.x + slope.x) % cols;
+    position.y += slope.y;
+    
+    if(lines[position.y] !== undefined && lines[position.y][position.x] === TileType.TREE){
+      trees++;
+    }
+    row++;
   }
-  console.log(row, position.y, lines[position.y][position.x]);
-  row++;
+
+  return trees;
 }
 
-console.log(trees);
+const ex1 = runThrough({x: 1, y: 1});
+const ex2 = runThrough({x: 3, y: 1});
+const ex3 = runThrough({x: 5, y: 1});
+const ex4 = runThrough({x: 7, y: 1});
+const ex5 = runThrough({x: 1, y: 2});
 
+console.log(ex1 * ex2 * ex3 * ex4 * ex5);
