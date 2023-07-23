834ef1f5d408107497e6e1506df31d7c9c6f1b60
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2023-07-23 11:13:56 +0200

    retry of day 7 in rust with the nom crate

--- aoc2015/day7/src/bin/part1.rs
+++ aoc2015/day7/src/bin/part1.rs
@@ -1,67 +1,32 @@
-#[derive(Debug)]
-enum Ops {
-    AND,
-    OR,
-    NOT,
-    LSHIFT,
-    RSHIFT,
+use nom::character::complete::alpha1;
+#[allow(unused_imports)]
+use nom::character::complete::{digit0, digit1};
+use nom::multi::many0;
+use nom::sequence::separated_pair;
+use nom::bytes::complete::tag;
+
+#[derive(Clone, Debug)]
+struct Line(String, String);
+
+#[allow(dead_code)]
+fn parse_many(line: &str) -> Result<(&str, Vec<(&str, &str)>), nom::Err<nom::error::Error<&str>>> {
+    many0(separated_pair(
+        digit1, 
+        tag(" -> "),
+        alpha1,
+    ))(line)
 }
 
-fn parse(line: &str) -> Vec<&str> {
-    let line = line.replace(" -> ", "|");
-    let statements: Vec<&str> = line.split('|').collect();
-    statements
-}
-
-fn get_tokens(line: &str) {
-    let tokens: Vec<&str> = line.split(' ').collect();
-    for (i, token) in tokens.iter().enumerate() {
-        if token.parse::<i32>().is_ok() {
-            let value = token.parse::<i32>().unwrap();
-            dbg!(value);
-        } else if token.len() == 1 {
-            let var = token.chars().next().unwrap();
-            dbg!(var);
-        } else if token == &"->" {
-            let dest = token;
-            let dest_var = &tokens.get(i + 1).expect("no destination variable");
-            dbg!(dest, dest_var);
-        } else {
-            let op = match token {
-                &"AND" => Some(Ops::AND),
-                &"OR" => Some(Ops::OR),
-                &"NOT" => Some(Ops::NOT),
-                &"LSHIFT" => Some(Ops::LSHIFT),
-                &"RSHIFT" => Some(Ops::RSHIFT),
-                _ => None,
-            };
-            dbg!(op.unwrap());
-        }
-    }
-}
-
-fn process(a: i32, b: i32, op: Ops) -> i32 {
-    match op {
-        Ops::AND => a & b,
-        Ops::OR => a | b,
-        Ops::NOT => ! a,
-        Ops::LSHIFT => a >> b,
-        Ops::RSHIFT => a << b,
-    }
+#[allow(dead_code)]
+fn parse_one(line: &str) -> Result<(&str, (&str, &str)), nom::Err<nom::error::Error<&str>>> {
+    separated_pair(
+        digit1,
+        tag(" -> "),
+        alpha1,
+    )(line)
 }
 
 fn main() {
-    let input = String::from("123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT y -> i");
-
-    for line in input.lines() {
-        let statements = parse(line);
-        // let tokens = get_tokens(line);
-    }
+    let line = parse_many("123 -> ab456 -> y");
+    dbg!("{}", line.unwrap());
 }
